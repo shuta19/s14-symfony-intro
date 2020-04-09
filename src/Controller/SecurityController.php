9e73c88438094ec3087d79d8cec459622c2d82bb
Romain Tello <romain.tello@gmail.com>
2020-04-09 10:38:00 +0200

    Add signup

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,13 +2,25 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class SecurityController extends AbstractController
 {
+    private $urlGenerator;
+
+    public function __construct(UrlGeneratorInterface $urlGenerator) {
+        $this->urlGenerator = $urlGenerator;
+    }
+
     /**
      * @Route("/login", name="app_login")
      */
@@ -32,5 +44,40 @@ class SecurityController extends AbstractController
     public function logout()
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    /**
+     * @Route("/signup", methods={"GET"}, name="app_signup")
+     */
+    public function signup()
+    {
+        return $this->render('security/signup.html.twig');
+    }
+
+    /**
+     * @Route("/signup", methods={"POST"}, name="app_process_signup")
+     */
+    public function processSignup(
+        Request $request,
+        UserPasswordEncoderInterface $passwordEncoder,
+        EntityManagerInterface $entityManager
+    )
+    {
+        $user = new User();
+
+        $user
+            ->setName($request->request->get('name'))
+            ->setEmail($request->request->get('email'))
+            ->setPassword($passwordEncoder->encodePassword(
+                $user,
+                $request->request->get('password')
+            ))
+        ;
+
+        $entityManager->persist($user);
+
+        $entityManager->flush();
+
+        return new RedirectResponse($this->urlGenerator->generate('app_login'));
     }
 }
